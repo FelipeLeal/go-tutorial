8d67f12b34fc10207f6344717a7af856388f60e0
FelipeLeal <f3lipel3al@gmail.com>
2020-11-23 23:07:30 -0300

    add two tests

--- hello/hello.go
+++ hello/hello.go
@@ -14,8 +14,11 @@ func main() {
 	log.SetPrefix("greetings: ")
 	// log.SetFlags(0)
 
+	// A slice of names.
+	names := []string{"Felipe", "Isidora", "Samantha", "Darrin"}
+
 	// Request a greeting message.
-	message, err := greetings.Hello("Felipe")
+	message, err := greetings.Hellos(names)
 	// If an error was returned, print it to the console and
 	// exit the program.
 	if err != nil {
